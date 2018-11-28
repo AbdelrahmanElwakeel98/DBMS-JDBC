da273b1720cf8054af2c0c9c8242e1333baa25f3
LENOVO <LENOVO@Molahez>
2018-11-28 13:56:21 +0200

    1st

--- src/eg/edu/alexu/csd/oop/db/cs03/InsertTable.java
+++ src/eg/edu/alexu/csd/oop/db/cs03/InsertTable.java
@@ -1,17 +1,27 @@
 package eg.edu.alexu.csd.oop.db.cs03;
 
+import java.io.File;
 import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 import eg.edu.alexu.csd.oop.db.CommandUpdate;
 
 public class InsertTable implements CommandUpdate {
-	
-	
+
 	private String databaseName;
 	private String tableName;
 	private ArrayList<IHolder> arrayOfinsert = new ArrayList<>();
-	
-	public InsertTable (String databaseName, String tableName, ArrayList<IHolder> arrayOfinsert){
+
+	public InsertTable(String databaseName, String tableName, ArrayList<IHolder> arrayOfinsert) {
 		this.databaseName = databaseName;
 		this.tableName = tableName;
 		this.arrayOfinsert = arrayOfinsert;
@@ -19,7 +29,46 @@ public class InsertTable implements CommandUpdate {
 
 	@Override
 	public void execute() {
-		
+
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder;
+
+		try {
+			dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.newDocument();
+
+			// root element
+			Element rootElement = doc.createElement("table");
+			doc.appendChild(rootElement);
+			// where j is number of columns
+
+			Element row = doc.createElement("row");
+			rootElement.appendChild(row);
+			for (int j = 0; j < arrayOfinsert.size(); j++) {
+				row.appendChild(Add_column(arrayOfinsert.get(j).getFieldNames(),arrayOfinsert.get(j).getDataTypes(), doc));
+			}
+
+			// write the content into xml file
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(new File("DataBases/" + this.databaseName + "/" + this.tableName + ".xml"));
+			transformer.transform(source, result);
+
+			// Output to console for testing
+			StreamResult consoleResult = new StreamResult(System.out);
+			transformer.transform(source, consoleResult);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static Element Add_column(String col_name, String col_data, Document doc) {
+		Element col = doc.createElement(col_name);
+		col.appendChild(doc.createTextNode(col_data));
+
+		return col;
 	}
 
 	@Override
--- src/eg/edu/alexu/csd/oop/db/cs03/test.java
+++ src/eg/edu/alexu/csd/oop/db/cs03/test.java
@@ -3,23 +3,30 @@ package eg.edu.alexu.csd.oop.db.cs03;
 import java.util.ArrayList;
 
 import eg.edu.alexu.csd.oop.db.Command;
+import eg.edu.alexu.csd.oop.db.CommandUpdate;
 
 public class test {
 	
 	public static void main(String[] args) {
 		
 		ArrayList<IHolder> arraylist = new ArrayList<>();
+		ArrayList<IHolder> arraylist1 = new ArrayList<>();
 		
 		arraylist.add(new IHolder("name", "VARCHAR"));
 		arraylist.add(new IHolder("phone", "VARCHAR"));
 		arraylist.add(new IHolder("id", "INT"));
+
+		arraylist1.add(new IHolder("name", "'ahmed'"));
+		arraylist1.add(new IHolder("phone", "'0111'"));
+		arraylist1.add(new IHolder("id", "4"));
 		
 		Command c = new CreateDatabase( "PoliceStation" );
 		Command c1 = new DropDatabase( "PoliceStation" );
 		Command c2 = new CreateTable("NewTable", "PoliceStation", arraylist);
 		Command c3 = new DropTable("NewTable", "PoliceStation");
+		CommandUpdate c4 = new InsertTable("NewTable", "PoliceStation", arraylist1);
 		
-		c3.execute();
+		c.execute();
 		
 	}
 }
