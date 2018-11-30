81d9cdb1ce9e182af073a3ab03c6f4434d8f240b
LENOVO <LENOVO@Molahez>
2018-11-30 18:41:51 +0200

    update

--- src/eg/edu/alexu/csd/oop/db/cs03/Update_table.java
+++ src/eg/edu/alexu/csd/oop/db/cs03/Update_table.java
@@ -4,26 +4,25 @@ import java.util.ArrayList;
 
 import eg.edu.alexu.csd.oop.db.Command;
 
-public class Delete_table implements Command {
-
+public class Update_table implements Command {
 	private String databaseName;
 	private String tableName;
 	private String col_name;
 	private String sign;
+	private ArrayList<IHolder> arrayOfinsert = new ArrayList<>();
 	private String value;
 	private Object[][] data;
 	private Dtdreader dtdreader;
 	private ArrayList<String> col_names;
 	private int col_num;
-	private int count_delete = 0;
-	private int count_new = 0;
 	private Save_table save_table;
 
-	public Delete_table(String databaseName, String tableName, Object[][] data, String col_name, String sign,
-			String value) {
+	public Update_table(String databaseName, String tableName, Object[][] data, String col_name, String sign,
+			String value, ArrayList<IHolder> arrayOfinsert) {
 		this.databaseName = databaseName;
 		this.tableName = tableName;
 		this.data = data;
+		this.arrayOfinsert = arrayOfinsert;
 		this.col_name = col_name;
 		this.sign = sign;
 		this.value = value;
@@ -33,7 +32,6 @@ public class Delete_table implements Command {
 	public Object execute() {
 		dtdreader = new Dtdreader(databaseName, tableName);
 		col_names = dtdreader.read();
-		Boolean[][] flag = new Boolean[data.length][data[0].length];
 		for (int i = 0; i < col_names.size(); i++) {
 			if (col_names.get(i).equals(col_name)) {
 				col_num = i;
@@ -45,9 +43,12 @@ public class Delete_table implements Command {
 				for (int j = 0; j < data[0].length; j++) {
 					if (j == col_num) {
 						if (value.equals((String) data[i][j])) {
-							count_delete++;
+
 							for (int k = 0; k < data[0].length; k++) {
-								flag[i][k] = true;
+								if (arrayOfinsert.get(k).getFieldNames().equals(col_names.get(k))) {
+									data[i][k] = arrayOfinsert.get(k).getDataTypes();
+								}
+
 							}
 						}
 					}
@@ -61,26 +62,31 @@ public class Delete_table implements Command {
 						switch (sign) {
 						case "<":
 							if ((int) data[i][j] < Integer.parseInt(value)) {
-								count_delete++;
+
 								for (int k = 0; k < data[0].length; k++) {
-									flag[i][k] = true;
+									if (arrayOfinsert.get(k).getFieldNames().equals(col_names.get(k))) {
+										data[i][k] = arrayOfinsert.get(k).getDataTypes();
+									}
+
 								}
 							}
 							break;
 						case ">":
 							if ((int) data[i][j] > Integer.parseInt(value)) {
-								count_delete++;
 								for (int k = 0; k < data[0].length; k++) {
-									flag[i][k] = true;
+									if (arrayOfinsert.get(k).getFieldNames().equals(col_names.get(k))) {
+										data[i][k] = arrayOfinsert.get(k).getDataTypes();
+									}
+
 								}
 							}
 							break;
 						case "=":
-							if ((int) data[i][j] == Integer.parseInt(value)) {
-								count_delete++;
-								for (int k = 0; k < data[0].length; k++) {
-									flag[i][k] = true;
+							for (int k = 0; k < data[0].length; k++) {
+								if (arrayOfinsert.get(k).getFieldNames().equals(col_names.get(k))) {
+									data[i][k] = arrayOfinsert.get(k).getDataTypes();
 								}
+
 							}
 							break;
 
@@ -89,19 +95,9 @@ public class Delete_table implements Command {
 				}
 			}
 		}
-		Object[][] new_data = new Object[data.length - count_delete][data[0].length];
-		for (int i = 0; i < data.length; i++) {
-			if (flag[i][0] == false) {
-				for (int j = 0; j < data[0].length; j++) {
-					new_data[count_new][j] = data[i][j];
-
-				}
-				count_new++;
-			}
-		}
-		save_table = new Save_table(databaseName, tableName, new_data);
+		save_table = new Save_table(databaseName, tableName, data);
 		save_table.save();
-		return new_data.length;
+		return data.length;
 	}
 
 }
