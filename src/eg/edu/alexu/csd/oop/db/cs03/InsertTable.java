d82d84a424038297ad04da0aaae9d384177f0b35
LENOVO <LENOVO@Molahez>
2018-11-30 17:32:38 +0200

    kk

--- src/eg/edu/alexu/csd/oop/db/cs03/InsertTable.java
+++ src/eg/edu/alexu/csd/oop/db/cs03/InsertTable.java
@@ -16,17 +16,20 @@ import org.w3c.dom.Element;
 
 import eg.edu.alexu.csd.oop.db.Command;
 
-
 public class InsertTable implements Command {
 
 	private String databaseName;
 	private String tableName;
 	private ArrayList<IHolder> arrayOfinsert = new ArrayList<>();
+	private Object[][] data;
+	private Dtdreader dtdreader;
+	private ArrayList<String> col_names;
 
-	public InsertTable(String databaseName, String tableName, ArrayList<IHolder> arrayOfinsert) {
+	public InsertTable(String databaseName, String tableName, ArrayList<IHolder> arrayOfinsert, Object[][] data) {
 		this.databaseName = databaseName;
 		this.tableName = tableName;
 		this.arrayOfinsert = arrayOfinsert;
+		this.data = data;
 	}
 
 	@Override
@@ -34,27 +37,41 @@ public class InsertTable implements Command {
 
 		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder dBuilder;
-
+		dtdreader = new Dtdreader(databaseName, tableName);
+		col_names = dtdreader.read();
 		try {
 			dBuilder = dbFactory.newDocumentBuilder();
 			Document doc = dBuilder.newDocument();
 
 			// root element
-			Element rootElement = doc.createElement("table");
+			Element rootElement = doc.createElement(tableName);
 			doc.appendChild(rootElement);
 			// where j is number of columns
 
 			Element row = doc.createElement("row");
 			rootElement.appendChild(row);
-			for (int j = 0; j < arrayOfinsert.size(); j++) {
-				row.appendChild(Add_column(arrayOfinsert.get(j).getFieldNames(),arrayOfinsert.get(j).getDataTypes(), doc));
+			if (data == null) {
+				for (int j = 0; j < arrayOfinsert.size(); j++) {
+					row.appendChild(Add_column(col_names.get(j), arrayOfinsert.get(j).getDataTypes(), doc));
+				}
+			} else {
+				for (int i = 0; i < data.length + 1; i++) {
+					for (int j = 0; j < arrayOfinsert.size(); j++) {
+						if (i == data.length) {
+							row.appendChild(Add_column(col_names.get(j), arrayOfinsert.get(j).getDataTypes(), doc));
+						} else {
+							row.appendChild(Add_column(col_names.get(j), (String) data[i][j], doc));
+						}
+					}
+				}
 			}
 
 			// write the content into xml file
 			TransformerFactory transformerFactory = TransformerFactory.newInstance();
 			Transformer transformer = transformerFactory.newTransformer();
 			DOMSource source = new DOMSource(doc);
-			StreamResult result = new StreamResult(new File("DataBases/" + this.databaseName + "/" + this.tableName + ".xml"));
+			StreamResult result = new StreamResult(
+					new File(databaseName + System.getProperty("file.separator") + tableName + ".xml"));
 			transformer.transform(source, result);
 
 			// Output to console for testing
@@ -63,7 +80,7 @@ public class InsertTable implements Command {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return true;
 
 	}
@@ -74,7 +91,5 @@ public class InsertTable implements Command {
 
 		return col;
 	}
-
-	
 
 }
--- src/eg/edu/alexu/csd/oop/db/cs03/Read_table.java
+++ src/eg/edu/alexu/csd/oop/db/cs03/Read_table.java
@@ -18,72 +18,52 @@ import org.w3c.dom.Element;
 
 public class Read_table {
 
-	public static void main(String[] args) {
-		Save_operation x = new Save_operation();
+	private String databaseName;
+	private String tableName;
+	private Dtdreader dtdreader;
+	private ArrayList<String> col_names;
+
+	public Read_table(String dbname, String tname) {
+		this.databaseName = dbname;
+		this.tableName = tname;
+	}
+
+	public Object[][] read_table() {
 		
-		File file = new File("file.dtd");
-		BufferedReader br;
-		int count = 0;
-		ArrayList<String> col_names = new ArrayList<String>();
-
+		dtdreader = new Dtdreader(databaseName,tableName);
+		col_names = dtdreader.read();
 		try {
-			br = new BufferedReader(new FileReader(file));
-			String st;
-			while ((st = br.readLine()) != null) {
-				if (count > 1) {
-
-					String pattern = "^<!ELEMENT+\\s+(\\w*+)\\s+(\\()+(#PCDATA)+(\\))+(\\>)$";
-					// Create a Pattern object
-					Pattern r = Pattern.compile(pattern);
-					// Now create matcher object.
-					Matcher m = r.matcher(st);
-					if (m.find()) {
-						col_names.add(m.group(1));
-					}
-				}
-				count++;
-
-			}
-
-		} catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		try {
-			File inputFile = new File("cars.xml");
+			File inputFile = new File(databaseName + System.getProperty("file.separator") + tableName + ".xml");
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-			Document doc = dBuilder.parse(inputFile);
-			doc.getDocumentElement().normalize();
-			System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-			NodeList nList = doc.getElementsByTagName("row");
-			System.out.println("----------------------------");
-			Object data[][] = new Object[nList.getLength()][col_names.size()];
+			if (inputFile.length() == 0) {
+				return null;
+			} else {
+				Document doc = dBuilder.parse(inputFile);
+				doc.getDocumentElement().normalize();
+				NodeList nList = doc.getElementsByTagName("row");
+				Object data[][] = new Object[nList.getLength()][col_names.size()];
 
-			for (int temp = 0; temp < nList.getLength(); temp++) {
-				Node nNode = nList.item(temp);
+				for (int temp = 0; temp < nList.getLength(); temp++) {
+					Node nNode = nList.item(temp);
 
-				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-					Element eElement = (Element) nNode;
-					for (int i = 0; i < col_names.size(); i++) {
-						data[temp][i] = eElement.getElementsByTagName(col_names.get(i)).item(0).getTextContent();
+					if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+						Element eElement = (Element) nNode;
+						for (int i = 0; i < col_names.size(); i++) {
+							data[temp][i] = eElement.getElementsByTagName(col_names.get(i)).item(0).getTextContent();
 
+						}
 					}
 				}
+				return data;
+
 			}
-			for (int i = 0; i < nList.getLength(); i++) {
-				for (int j = 0; j < col_names.size(); j++) {
-					System.out.println(data[i][j]);
-				}
-			}
-			x.save(data, "");
 		} catch (Exception e) {
+
 			e.printStackTrace();
+			return null;
 		}
+
 	}
 
 }
